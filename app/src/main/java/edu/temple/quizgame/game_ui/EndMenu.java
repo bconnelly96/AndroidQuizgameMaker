fa3f180b4293260542d83236b7a06ff8e90c9f89
bconnelly <tug21498@temple.edu>
2018-11-15 14:33:52 -0500

    fixed layout files

--- app/src/main/java/edu/temple/quizgame/game_ui/EndMenu.java
+++ app/src/main/java/edu/temple/quizgame/game_ui/EndMenu.java
@@ -1,7 +1,9 @@
 package edu.temple.quizgame.game_ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import edu.temple.quizgame.R;
 
